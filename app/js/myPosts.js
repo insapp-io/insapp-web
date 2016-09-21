fa63da21b245b020d7cd25b532c67243d3bfc4e2
fthomasmorel <florent@thomasmorel.io>
2016-09-21 18:33:55 +0200

    Refactor Post

--- app/js/myPosts.js
+++ app/js/myPosts.js
@@ -1,7 +1,6 @@
 app.controller('MyPosts', ['$scope', '$resource', '$location', 'Session', function($scope, $resource, $location, Session) {
   var Association = $resource('https://api.thomasmorel.io/association/:id?token=:token');
   var Post = $resource('https://api.thomasmorel.io/post/:id?token=:token');
-  var Event = $resource('https://api.thomasmorel.io/event/:id?token=:token');
 
   if(Session.getToken() == null || Session.getAssociation() == null){
     $location.path('/login')
@@ -10,7 +9,7 @@ app.controller('MyPosts', ['$scope', '$resource', '$location', 'Session', functi
   $scope.isActive = function (viewLocation) {
       return viewLocation === $location.path();
   };
-  
+
   Association.get({id:Session.getAssociation(), token:Session.getToken()}, function(assos) {
     $scope.myPosts = []
     assos.posts = (assos.posts == null ? [] : assos.posts)
@@ -19,10 +18,7 @@ app.controller('MyPosts', ['$scope', '$resource', '$location', 'Session', functi
         post.nbLikes = (post.likes != null ? post.likes.length : 0)
         post.nbComments = (post.comments != null ? post.comments.length : 0)
         post.associationName = assos.name
-        Event.get({id:post.event, token:Session.getToken()}, function(event){
-          post.eventName = event.name
-          $scope.myPosts.push(post)
-        });
+        $scope.myPosts.push(post)
       });
     }
   }, function(error) {
