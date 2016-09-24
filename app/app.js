36ee85ef5a564819e7cd2884a1350772813323c1
fthomasmorel <florent@thomasmorel.io>
2016-09-24 13:38:37 +0200

    change URL

--- app/app.js
+++ app/app.js
@@ -1,7 +1,7 @@
 var app = angular.module('insapp', ['ngRoute','ngResource','ui.bootstrap.datetimepicker', 'ngFileUpload', 'ngDialog', 'ngColorThief', 'ngFileUpload', 'ngLoadingOverlay','angular-spinkit']);
 
 app.config(['$loadingOverlayConfigProvider', function ($loadingOverlayConfigProvider) {
-    $loadingOverlayConfigProvider.defaultConfig('<img style="display: block;margin-left: auto;margin-right: auto; width:50px" src="images/loader.gif"></img><h3>Chargement</h3>', 'rgba(0, 0, 0, 0.5)', '#fff');
+    $loadingOverlayConfigProvider.defaultConfig('<img style="display: block;margin-left: auto;margin-right: auto; width:50px" src="/web/images/loader.gif"></img><h3>Chargement</h3>', 'rgba(0, 0, 0, 0.5)', '#fff');
   }
 ]);
 
