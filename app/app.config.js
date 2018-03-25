f34359caca0e208ebd050ab1eab72a41187e4bbc
Thomas Bouvier <thomas.bouvier@insa-rennes.fr>
2018-03-25 19:31:33 +0200

    Updated default config

--- app/app.config.js
+++ app/app.config.js
@@ -1,7 +1,7 @@
 var app = angular.module('insapp', ['ngRoute','ngResource','ui.bootstrap.datetimepicker', 'ngFileUpload', 'ngDialog', 'ngFileUpload', 'ngLoadingOverlay','angular-spinkit']);
 
 app.constant('configuration', {
-  api: 'http://localhost:9010/api/v1',
-  cdn: 'http://localhost:9010/cdn/',
+  api: 'https://insapp.fr/api/v1',
+  cdn: 'https://insapp.fr/cdn/',
   baseUrl: '/web',
 });
